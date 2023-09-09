426ac4803482f57b839c0b0a3f1faa8fb6f0258a
Ilhaam <ilhaam777@gmail.com>
2023-09-09 16:24:27 +0100

    "string interpolation done"

---
+++ string_interpolation.py
@@ -0,0 +1,24 @@
+# %-formatting
+name = "Ilhaam"
+print("Hello, %s!" % name)
+
+# str.format()
+name = "Ilhaam"
+print("Hello, {}!".format(name))
+
+# String Concatenation
+name = "Ilhaam"
+print("Hello, " + name + "!")
+
+
+# Make this program print:
+# One Sunday morning the warm sun came up and - pop!
+
+day = "Sunday"
+time_of_day = "morning"
+star_quality = "warm"
+star_name = "sun"
+star_direction = "up"
+mysterious_sound = "pop"
+
+print("One {} {} the {} {} came {} and - {}!".format(day, time_of_day, star_quality, star_name, star_direction, mysterious_sound))
